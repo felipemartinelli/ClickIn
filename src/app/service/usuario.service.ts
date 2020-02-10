2342e566287071c252ee4864966cca32e1222843
viviane-scarlatti <viviane.scarlatti@gmail.com>
2020-02-10 11:29:53 -0300

    projeto final

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -11,45 +11,45 @@ export class UsuarioService {
   constructor(public http: HttpClient) { }
 
   public recuperaTodos(){
-    return this.http.get("http://clickin.site:8080/usuario/todos");
+    return this.http.get("http://localhost:8080/usuario/todos");
 
   }
   public recuperaPostsUsuario(id: number){
-    return this.http.get("http://clickin.site:8080/usuario/"+id);
+    return this.http.get("http://localhost:8080/usuario/"+id);
 
   }
   public insere(usuario:Usuario){
-    return this.http.post("http://clickin.site:8080/usuario/novo",usuario);
+    return this.http.post("http://localhost:8080/usuario/novo",usuario);
   }
 
   public atualiza(usuario: Usuario){
-    return this.http.put("http://clickin.site:8080/usuario", usuario);
+    return this.http.put("http://localhost:8080/usuario", usuario);
   }
 
   public autenticar(usuario: Usuario) {
-    return this.http.post("http://clickin.site:8080/usuario/login", usuario);
+    return this.http.post("http://localhost:8080/usuario/login", usuario);
   }
 
   public buscarInfo(token: string){
-    return this.http.get("http://clickin.site:8080/usuario/infoDoUsuario?token="+token);
+    return this.http.get("http://localhost:8080/usuario/infoDoUsuario?token="+token);
   }
 
   public  insereInfoUsuario(infousuario:InfoUsuario){
-    return this.http.post("http://clickin.site:8080/infoUsuario/novo",infousuario);
+    return this.http.post("http://localhost:8080/infoUsuario/novo",infousuario);
   }
 
   public recuperaInfoUsuario(id: number){
-    return this.http.get("http://clickin.site:8080/infoUsuario/"+id);
+    return this.http.get("http://localhost:8080/infoUsuario/"+id);
 
   }
 
   public recuperaUsuario(id: number){
-    return this.http.get("http://clickin.site:8080/usuario/"+id);
+    return this.http.get("http://localhost:8080/usuario/"+id);
 
   }
 
   public alteraInfoUsuario(infousuario: InfoUsuario){
-    return this.http.put("http://clickin.site:8080/infoUsuario/alterar", infousuario);
+    return this.http.put("http://localhost:8080/infoUsuario/alterar", infousuario);
   }
 
   
